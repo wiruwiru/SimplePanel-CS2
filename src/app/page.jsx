65d39125960f90607a84a49c1bda4d1724b0fb38
WiruWiru <61034981+wiruwiru@users.noreply.github.com>
2025-11-09 18:04:30 -0300

    fix: remove recent sanctions componentes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,5 @@
 import { SanctionsStats } from "@/components/Home/SanctionsStats"
 import { ServersList } from "@/components/Home/ServersList"
-import { RecentBans } from "@/components/Home/RecentBans"
-import { RecentMutes } from "@/components/Home/RecentMutes"
 
 export const metadata = {
   title: "Inicio | CrisisGamer"
@@ -10,14 +8,8 @@ export const metadata = {
 export default function Home() {
   return (
     <div className="space-y-4 md:space-y-6 max-w-7xl mx-auto mt-6">
-
       <SanctionsStats />
       <ServersList />
-
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-6">
-        <RecentBans />
-        <RecentMutes />
-      </div>
     </div>
   )
 }
